a56721dd087e4e981135a0b8e8a11b4558ed23dd
bta <bertrand.taboulet@gmail.com>
2022-03-29 21:01:08 +0200

    init navigation components routes + button

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,15 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layer/Header';
-import "bootstrap/dist/css/bootstrap.min.css"
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <Dashboard />
-    </div>
+    
+      
+        <div className="App">
+            <Header />
+            <Router>
+              <Routes>
+                <Route exact path="/" element={<Dashboard />} />  
+                <Route exact path="/dashboard" element={<Dashboard />} />
+                <Route exact path="/addProject" element={<AddProject />} />
+              </Routes>            
+            </Router>
+        </div>
+    
   );
 }
 
