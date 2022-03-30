c074981d4d497d9ca5f8b4e776c672f43f151888
bta <bertrand.taboulet@gmail.com>
2022-03-30 17:07:33 +0200

    downgrade package versions : pb avec le this.props.history

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,28 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layer/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
 import {Provider} from "react-redux";
 import store from './store';
 
-function App() {
-  return (    
+class App extends Component {
+  render() {
+    return (
       <Provider store={store}>
-        <div className="App">
+        <Router>
+          <div className="App">
             <Header />
-            <Router>
-              <Routes>
-                <Route exact path="/" element={<Dashboard />} />  
-                <Route exact path="/dashboard" element={<Dashboard />} />
-                <Route exact path="/addProject" element={<AddProject />} />
-              </Routes>            
-            </Router>
-        </div>
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
       </Provider>
-  );
+    );
+  };
 }
 
 export default App;
